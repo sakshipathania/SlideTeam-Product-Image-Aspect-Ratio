aff6ae328e328494a6a63c0ed018f87a7d7eeaba
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2021-06-07 16:36:24 +0530

    Update sign_up_correct_data.java

--- src/test/java/SetupClass/TestStep/sign_up_correct_data.java
+++ src/test/java/SetupClass/TestStep/sign_up_correct_data.java
@@ -149,7 +149,7 @@ public class sign_up_correct_data extends Set {
 		//WebElement free_ppt_btn=wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[2]/header/div[1]/div[2]/div/nav/div/div/ul/li[2]/div/ul/li[1]/a")));
 	    //free_ppt_btn.click();
 	    //Thread.sleep(2000);
-		WebElement size=driver.findElement(By.xpath("/html/body/div[2]/main/div[2]/div/div[4]/div[2]/ol/li[1]/div/a/img"));
+		WebElement size=driver.findElement(By.className("small_image_hover"));
 	        Thread.sleep(2000);
 		Dimension size1 = size.getSize();
     //   int width=driver.findElement(By.tagName("img")).getSize().getWidth();
@@ -166,7 +166,7 @@ public class sign_up_correct_data extends Set {
 	driver.get("https://www.slideteam.net/professional-powerpoint-templates");
 		Thread.sleep(5000);
 		
-	WebElement size0=driver.findElement(By.xpath("/html/body/div[2]/main/div[2]/div/div[4]/div[2]/ol/li[1]/div/a/img"));
+	WebElement size0=driver.findElement(By.className("small_image_hover"));
 		Thread.sleep(2000);
      // int height1=driver.findElement(By.tagName("img")).getSize().getHeight();
 Dimension size2 = size0.getSize();

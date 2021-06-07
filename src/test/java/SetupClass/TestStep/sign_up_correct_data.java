2e08b839a6c7bce54dffc9fe9fad163fd3ffce35
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2021-06-07 13:54:24 +0530

    Update sign_up_correct_data.java

--- src/test/java/SetupClass/TestStep/sign_up_correct_data.java
+++ src/test/java/SetupClass/TestStep/sign_up_correct_data.java
@@ -149,7 +149,7 @@ public class sign_up_correct_data extends Set {
 		//WebElement free_ppt_btn=wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[2]/header/div[1]/div[2]/div/nav/div/div/ul/li[2]/div/ul/li[1]/a")));
 	    //free_ppt_btn.click();
 	    //Thread.sleep(2000);
-		WebElement size=driver.findElement(By.xpath("//a[@title='Business Puzzle List Diagarm Free PowerPoint Templates Download']"));
+		WebElement size=driver.findElement(By.xpath("//img[@title='Business Puzzle List Diagarm Free PowerPoint Templates Download']"));
 	        Thread.sleep(2000);
 		Dimension size1 = size.getSize();
     //   int width=driver.findElement(By.tagName("img")).getSize().getWidth();
@@ -166,7 +166,7 @@ public class sign_up_correct_data extends Set {
 	driver.get("https://www.slideteam.net/professional-powerpoint-templates");
 		Thread.sleep(5000);
 		
-	WebElement size0=driver.findElement(By.xpath("//a[@title='Change Management Powerpoint Presentation Slides']"));
+	WebElement size0=driver.findElement(By.cssSelector("//img[@title='Change Management Powerpoint Presentation Slides']"));
 		Thread.sleep(2000);
      // int height1=driver.findElement(By.tagName("img")).getSize().getHeight();
 Dimension size2 = size0.getSize();
